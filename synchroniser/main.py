be0a13482cd7bb7b5b32ad66546fe7b52c63cf50
benji.k@live.k@live.com.au <benji.k@live.k@live.com.au>
2019-07-06 19:53:33 +1000

    fix cmd to download

--- synchroniser/main.py
+++ synchroniser/main.py
@@ -1,77 +1,65 @@
-# pip install paramiko
-from ftplib import FTP
+# pip3 install paramiko
+# pip3 install pysftp
+import pysftp
 import os
 import fileinput
 import paramiko
 import time
 
-# Send all files
+
+def upload_files(server):
+    with pysftp.Connection(server[0], username=server[1], password=server[2]) as sftp:
+        out_dir = 'Light-Show-Toolkit-/build/'
+        with sftp.cd(out_dir):
+            in_dir = 'build/src/songs/'
+            for filename in os.listdir(in_dir):
+                filepath = os.path.join(in_dir, filename)
+                print("Uploading: " + filepath)
+                sftp.put(filepath)
 
 
-server = "10.0.0.50"
-username = "pi"
-password = "raspberry"
-servers = [(server, username, password)]
+def run_command(command, ssh):
+    print("Command: " + command)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command + '\n')
+    print(ssh_stderr.read())
+    print(ssh_stdout.read())
 
 
-def sync_files(servers):
-    ftp = FTP()
-    ftp.set_debuglevel(2)
+def prepare_pis(server):
+
+    ssh = paramiko.SSHClient()
+    ssh.load_system_host_keys()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(server[0], username=server[1], password=server[2])
+
+    commands = [
+        "sudo apt-get install cmake git libsdl2-dev libsdl2-net-dev libsdl2-mixer-dev libsdl2-2.0",
+        "git clone https://github.com/BKrajancic/Light-Show-Toolkit-.git;",
+        "cd Light-Show-Toolkit-/;git pull;git pull --recurse-submodules;git submodule update --init --recursive;",
+        "cd Light-Show-Toolkit-/;mkdir build;cd build;cmake ../",
+        "cd Light-Show-Toolkit-/RF24;sudo ./configure;make all",
+        "cd Light-Show-Toolkit-/build/src/;make all"
+    ]
+
+    for command in commands:
+        print("-------")
+        run_command(command, ssh)
+        print("-------")
+
+
+if __name__ == "__main__":
+    servers = [
+        ("10.0.0.50", "pi", "raspberry"),
+        ("10.0.0.18", "pi", "raspberry")
+    ]
+
     for server in servers:
-        ftp.connect(server[0], 21)
-        ftp.login(server[1], server[2])
-        ftp.cwd('/')
-
-        for filename in os.listdir('../build/src/songs/'):
-            fp = open(filename, 'rb')
-            ftp.storbinary('STOR %s' % os.path.basename(filename), fp, 1024)
-            fp.close()
-
-
-ssh = paramiko.SSHClient()
-ssh.load_system_host_keys()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(server, username=username, password=password)
-
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(
-    "sudo apt-get install cmake git libsdl2-dev libsdl2-net-dev libsdl2-mixer-dev libsdl2-2.0" + '\n')
-print(ssh_stderr.read())
-print(ssh_stdout.read())
-print("")
-
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(
-    "git clone https://github.com/BKrajancic/Light-Show-Toolkit-.git;  " + '\n')
-print(ssh_stderr.read())
-print(ssh_stdout.read())
-print("")
-
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(
-    "cd Light-Show-Toolkit-/;git pull;git submodule update --init --recursive;git pull --recurse-submodules")
-print(ssh_stderr.read())
-print(ssh_stdout.read())
-print("")
-
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(
-    "cd Light-Show-Toolkit-/;mkdir build;cd build;cmake ../" + '\n')
-print(ssh_stderr.read())
-print(ssh_stdout.read())
-print("")
-
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(
-    "cd Light-Show-Toolkit-/RF24;sudo ./configure;make all" + '\n')
-print(ssh_stderr.read())
-print(ssh_stdout.read())
-print("")
-
-ssh_stdin.close()
-
-
-#channel = ssh.get_transport().open_session()
-
-# channel.get_pty()         # get a PTY
-# channel.invoke_shell()    # start the shell before sending commands
-#channel.send("git clone https://github.com/BKrajancic/Light-Show-Toolkit-.git")
-
-# print(channel.recv(1024))
-# Other commands you must run
-#"sudo apt-get install git"
+        print("====================================")
+        print("Executing on server: " + str(server))
+        print("-----------------")
+        print("Running Commands")
+        prepare_pis(server)
+        print("-----------------")
+        print("Uploading Files")
+        upload_files(server)
+        print("====================================")
